b6b45f3b183a05d191c12646c79f8d524159818e
Alexander Duong <Alexander.Q.Duong@gmail.com>
2018-11-27 18:28:32 -0800

    Updated world and object teleportation
    Fixed that weird bug where the tiles wouldn't teleport
    Teleport objects that pass the world boundry
    Updated textures

--- Assets/Scripts/DestroyByTime.cs
+++ Assets/Scripts/DestroyByTime.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 /*
  * Simple script that automatically destroys a game object after a set amount of time.
+ * Set to -1 to make it last forever
  */
 public class DestroyByTime : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class DestroyByTime : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        if (lifetime == -1)
+            return;
         Destroy(gameObject, lifetime);
     }
 }
---
+++ Assets/Scripts/WorldTileEdgeTrigger.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTileEdgeTrigger : MonoBehaviour {
+
+    public WorldController worldController;
+
+    private ContactFilter2D filter;
+    private Collider2D col;
+
+    // Use this for initialization
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        filter.NoFilter();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Middle tile detection (Shift world)
+        if (this.gameObject == worldController.worldTiles[1])
+        {
+            // Ignore everything that isn't the player
+            if (other.tag != "Player")
+                return;
+
+            // Right boundary
+            if (other.transform.position.x > transform.position.x)
+                worldController.shiftWorldRight();
+            // Left boundary
+            else
+                worldController.shiftWorldLeft();
+        }
+
+        // Left tile detection (Teleport objects to the right)
+        else if (this.gameObject == worldController.worldTiles[0])
+        {
+            // Ignore right boundary triggers
+            if (other.transform.position.x > transform.position.x)
+                return;
+
+            // Teleport object to the other side
+            other.transform.SetPositionAndRotation(
+                other.transform.position + new Vector3(col.bounds.extents.x * 6, 0, 0), 
+                other.transform.rotation);
+        }
+
+        // Right tile detection (Teleport objects to the left)
+        else if (this.gameObject == worldController.worldTiles[2])
+        {
+            // Ignore left boundary triggers
+            if (other.transform.position.x < transform.position.x)
+                return;
+
+            // Teleport object to the other side
+            other.transform.SetPositionAndRotation(
+                other.transform.position - new Vector3(col.bounds.extents.x * 6, 0, 0),
+                other.transform.rotation);
+        }
+    }
+}
