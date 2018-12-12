30623ab9e1af6cbd931e50975b0668a5b8d2e083
Howard Wang <howard.wang@sjsu.edu>
2018-12-11 19:31:44 -0800

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string playGameLevel;
+
+	// Function to Play Game
+	public void playGame () {
+        Debug.Log("pressed");
+        SceneManager.LoadScene(playGameLevel);
+	}
+}
